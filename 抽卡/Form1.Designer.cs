df5138eb7a61467b959493dc83f770561d8978bc
許福陞 <zxcv81602@gmail.com>
2021-01-13 02:22:11 +0800

    差外觀 設計圖

--- 抽卡/Form1.Designer.cs
+++ 抽卡/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bt1
+            // 
+            this.bt1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bt1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.bt1.BackgroundImage = global::抽卡.Properties.Resources.抽卡按鈕;
+            this.bt1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.bt1.Location = new System.Drawing.Point(795, 401);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(120, 120);
+            this.bt1.TabIndex = 0;
+            this.bt1.UseVisualStyleBackColor = false;
+            this.bt1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pb1
+            // 
+            this.pb1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pb1.Location = new System.Drawing.Point(12, 80);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(766, 442);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb1.TabIndex = 1;
+            this.pb1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(941, 534);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.bt1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.PictureBox pb1;
     }
 }
 
--- 抽卡/Form1.cs
+++ 抽卡/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +18,68 @@ namespace 抽卡
         public Form1()
         {
             InitializeComponent();
+        }
+        public class resp
+        {
+            public List<img> data { get; set; }
+        }
+
+        public class img
+        {
+            public string link { get; set; }
+        }
+
+        private resp GetImages(string albumHash, string clientId)
+        {
+            resp result = null;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.imgur.com/3/album/0PxBYNN/images");
+
+                WebHeaderCollection myWebHeaderCollection = request.Headers;
+                myWebHeaderCollection.Add(albumHash, "Client-ID " + clientId);
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+                string json = readStream.ReadToEnd();
+                result = JsonConvert.DeserializeObject<resp>(json);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.ToString());
+                throw;
+            }
+            return result;
+        }
+
+        private Image Getimagefromurl(string url)
+        {
+            Image result = null;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream receiveStream = response.GetResponseStream();
+                result = Image.FromStream(receiveStream);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.ToString());
+                throw;
+            }
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var m = GetImages("Authorization", "4c73ffd69840e6e");
+            Random girl = new Random();
+            if (m == null)
+            {
+                return;
+            }
+            pb1.Image = Getimagefromurl(m.data[girl.Next(0,77)].link);
         }
     }
 }
